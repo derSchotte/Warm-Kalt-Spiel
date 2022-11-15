b1e50e7fc00120147bb0f7294b52787e43964f26
Frank McDonald <kontakt@frank-mcdonald.de>
2022-11-15 07:38:59 +0100

    ReadMe aktuallisiert, Progrm erstellt.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,127 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine( "Hello, World!" );
+﻿internal class WarmKaltSpiel {
+    static int alteEingabe = 0;
+    static int gesucht;
+    static bool Hardcore;
+    static int counter = 1;
+    static string[,] normalScore = new string[10,2];
+    static string[,] hardcoreScore = new string[10,2];
+
+    public static void Main( string[] args) {
+        Gewonnen();
+
+        // Optional: Zufallszahl (1-1000), Highscore
+        // - zwei Highscore Arrays für 1-100 und 1-1000
+        // - Namen eintragen in Highscore am ende des Games
+        // - Speichern der Highscore in eine json oder txt
+
+
+    }
+
+    private static void GameMode() {
+        // - Abfrage normal oder Hardcore
+        string eingabe;
+
+        Console.WriteLine( "Welchen Modus wollen sie Spielen?" );
+        Console.WriteLine( "(Normal [1-100]) oder (Hardcore [1-1000])" );
+        eingabe = Console.ReadLine();
+
+        if( eingabe.ToLower() == "normal" ) {
+            Hardcore = false;
+        } else if( eingabe.ToLower() == "hardcore" ) {
+            Hardcore = true;
+        }
+    }
+
+    private static void Gewonnen() {
+        bool gewonnen = false;
+        GameMode();
+
+        gesucht = GenerateRandom();
+
+        do {
+            if( Ueberpruefen( Eingaben() ) ) {
+                gewonnen = true;
+            }
+        } while( !gewonnen );
+
+        //EingabeHighscore();
+    }
+
+    private static void EingabeHighscore() {
+        string name;
+        string versuche;
+
+        do {
+            Console.WriteLine( "Geben sie 3 Zeichen als Namen ein!" );
+            name = Console.ReadLine();
+        } while( name.Length != 3 );
+
+        versuche = counter.ToString();
+
+        //if(Hardcore == true) {
+        //    hardcoreScore = hardcoreScore.Concat(new string[,] { { name, versuche } } ).ToArray();
+        //}
+    }
+
+    // Zufallszahl generieren zwischen min und max, wobei min und max aktuell feste werte sind.
+    private static int GenerateRandom() {
+        Random random = new Random();
+
+        if( Hardcore == true ) {
+            return random.Next( 1, 1001 );
+        } else {
+            return random.Next( 1, 101 );
+        }
+    }
+
+    private static int Eingaben() {
+        // User-Eingabe anlegen: DAU, Ersteingabe, Treffer?
+        int eingabe = 0;
+        bool check;
+        Console.WriteLine( $"Versuche: {counter}" );
+
+        do {
+            if( Hardcore == true ) {
+                Console.WriteLine( "Gib eine Zahl ein zwischen 1 und 1000" );
+            } else {
+                Console.WriteLine( "Gib eine Zahl ein zwischen 1 und 100" );
+            }
+
+            check = int.TryParse( Console.ReadLine(), out eingabe );
+
+            if( !check ) {
+                Console.WriteLine( "Gib eine Zahl ein... z.B. sowas wie 42!\n" );
+            } else if( eingabe == 0 ) {
+                Console.WriteLine( "Spassvogel, aber Ok es war ein Versuch" );
+            }
+
+        } while( !check );
+
+        counter++;
+        return eingabe;
+    }
+
+    private static bool Ueberpruefen( int eingabe ) {
+        if( eingabe == gesucht ) {
+            if( gesucht == 42 ) {
+                Console.WriteLine( $"Die Antwort auf alle Fragen: {gesucht}" );
+            } else {
+                Console.WriteLine( $"Super, die eingabe {eingabe} ist gleich {gesucht}" );
+            }
+            return true;
+        }
+
+        int alt = Math.Abs(alteEingabe - gesucht);
+        int neu = Math.Abs(eingabe - gesucht);
+
+        if( alt > neu ) {
+            Console.WriteLine( $"WÄRMER..." );
+        } else if( alt < neu ) {
+            Console.WriteLine( $"KÄLTER..." );
+        }
+
+        alteEingabe = eingabe;
+
+        return false;
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # Warm-Kalt-Spiel
+
+## Umschulung 2022 FI-AE - LuG Academy
+
+### Aufgabe:
+
+Schreiben Sie ein Programm, welches das Spiel "Warm oder Kalt" simuliert.
+
+Das soll wie folgt funktionieren:
+
+- Das Programm bestimmt eine zufällige ganze Zahl zwischen 1 und 100
+
+- Der Spieler/Anwender soll diese Zahl nun erraten, indem er eine Zahl
+  eingibt
+
+- nun wird der vorherige Versuch (falls es einen gab, es also nicht der
+  erste Versuch war) mit dem aktuellen Versuch verglichen werden:
+  - ist man näher dran als bei dem vorherigen Versuch: wärmer
+  - ist man weiter weg als bei dem vorherigen Versuch: kälter
+
+- Errät der Spieler die Zahl, hat er gewonnen
+
+- Nachdem die Zahl erraten wurde, soll die Anzahl der Versuche ausgegeben
+  werden (damit man zu anderen Spielern einen Vergleich hat)
+
+- Am Ende des Programms soll der Spieler gefragt werden, ob er noch eine
+  Partie spielen möchte
+
+Optional:
+---------
+- Am Anfang des Programms kann der Spieler auch einen "Hardmode" auswählen.
+  Das bedeutet, dass die gesuchte Zahl zwischen 1 und 1000 liegt
+
+- Fügen Sie eine Highscore hinzu um den Spieler mit den wenigsten Versuchen
+  zu finden.
